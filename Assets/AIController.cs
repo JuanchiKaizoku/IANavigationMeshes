1147ec80a5f61ffe6e001fbef3466e2ee77126c8
Juan Carlos Lopez Parapar <juanchi9619@gmail.com>
2025-02-02 19:49:13 +0100

    NavMesh END

--- Assets/AIController.cs
+++ Assets/AIController.cs
@@ -7,6 +7,7 @@ public class AIController : MonoBehaviour
 {
 
     public NavMeshAgent agent;
+    public GameObject target;
     Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,6 +20,7 @@ public class AIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(target.transform.position);
         if (agent.remainingDistance < 2){
             anim.SetBool("isMoving", false);
         }
